415fc87eb2b89b0f5ce74490c62b36ab96de95fb
Ubuntu <ubuntu@ip-172-31-41-192.ap-south-1.compute.internal>
2020-04-20 19:04:00 +0000

    added and updated o=all the files

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,6 +6,8 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+docker tag development anumshr/development
+dockerpath=anumshr/development
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +15,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
